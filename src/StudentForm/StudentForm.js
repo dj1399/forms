64e453ca9b58b5788d5f425e19aae004e963c9f5
Srushti <srushti.raybhoge1999@gmail.com>
2021-02-09 20:38:02 +0530

    integration done

--- src/StudentForm/StudentForm.js
+++ src/StudentForm/StudentForm.js
@@ -5,7 +5,8 @@ import Date from "./date";
 import arrow from "../Images/arrow.svg";
 import page2 from "../Images/page2.png";
 import page1 from "../Images/page1.png";
-
+import { sendStudentForm } from "../DataService/StudentForm";
+import Recaptcha from "react-google-invisible-recaptcha";
 export default class StudentForm extends React.Component {
   constructor(props) {
     super(props);
@@ -18,8 +19,26 @@ export default class StudentForm extends React.Component {
       color2: "#29517C",
       borderColor1: "#29517C",
       borderColor2: "transparent",
+      messageSent: false,
+      firstName: "",
+      middleName: "",
+      lastName: "",
+      address: "",
+      DateOfBirth: "",
+      bloodGroup: "",
+      gender: "",
+      contactNo: "",
+      alternateNo: "",
+      email: "",
+      collegeName: "",
+      yearOfPassing: "",
+      memberType: "",
     };
   }
+  onResolved = () => {
+    this.setState({ messageSent: true });
+    console.log(this.state);
+  };
   handleClickNext = (event) => {
     event.preventDefault();
     this.setState({
@@ -59,9 +78,50 @@ export default class StudentForm extends React.Component {
       color2: "white",
       borderColor1: "#29517C",
       borderColor2: "#29517C",
+      firstName: "",
     });
   };
+  handleChange = (event) => {
+    event.preventDefault();
+    // let key = event.target.name;
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+  handleSubmit = (event) => {
+    console.log(this.state);
+    event.preventDefault();
+    this.recaptcha.execute();
 
+    const data = new FormData();
+    data["firstName"] = this.state.firstName;
+    data["lastName"] = this.state.lastName;
+    data["middleName"] = this.state.middleName;
+    data["address"] = this.state.address;
+    data["DateOfBirth"] = this.state.DateOfBirth;
+    data["bloodGroup"] = this.state.bloodGroup;
+    data["contactNo"] = this.state.contactNo;
+    data["alternateNo"] = this.state.alternateNo;
+    data["gender"] = this.state.gender;
+    data["email"] = this.state.email;
+    data["collegeName"] = this.state.collegeName;
+    data["yearOfPassing"] = this.state.yearOfPassing;
+    data["memberType"] = this.state.memberType;
+
+    console.log(data);
+    sendStudentForm(data)
+      .then((response) => {
+        console.log(response.data);
+        if (response.data.status === "success") {
+          alert("Success " + response.data.message);
+          window.location.reload();
+        }
+      })
+      .catch((err) => {
+        alert(err);
+        console.log(err);
+      });
+  };
   render() {
     return (
       <div className="Student form_main">
@@ -87,148 +147,153 @@ export default class StudentForm extends React.Component {
                   </div>
 
                   <div className="form-row row1 ">
-                    <div className="col-md-4 col-12 no-gutters ">
+                    <div className="col-md-4 col-12 no-gutters StudentMobile_mp">
                       <input
                         type="text"
-                        class="form-control "
-                        id="Student_input"
+                        className="form-control Student_input"
+                        id="firstName"
+                        name="firstName"
                         placeholder="First Name"
+                        required
+                        onChange={this.handleChange}
+                        pattern="[A-Za-z_ ]{1,32}"
+                        title="*ENTER CHARACTER VALUES ONLY"
                       />
                     </div>
-                    <div className="col-md-4 col-12 no-gutters">
+                    <div className="col-md-4 col-12 no-gutters StudentMobile_mp">
                       <input
                         type="text"
-                        class="form-control "
-                        id="Student_input"
+                        class="form-control Student_input"
+                        id="middleName"
+                        name="middleName"
                         placeholder="Middle Name"
+                        required
+                        onChange={this.handleChange}
+                        pattern="[A-Za-z_ ]{1,32}"
+                        title="*ENTER CHARACTER VALUES ONLY"
                       />
                     </div>
-                    <div className="col-md-4 col-12 no-gutters">
+                    <div className="col-md-4 col-12 no-gutters StudentMobile_mp">
                       <input
                         type="text"
-                        class="form-control "
-                        id="Student_input"
+                        class="form-control Student_input"
+                        id="lastName"
+                        name="lastName"
                         placeholder="Last Name"
+                        required
+                        onChange={this.handleChange}
+                        pattern="[A-Za-z_ ]{1,32}"
+                        title="*ENTER CHARACTER VALUES ONLY"
                       />
                     </div>
                   </div>
                   <div className="form-row row2">
                     <textarea
-                      className="Student_add"
+                      className="Student_add StudentMobile_mp"
+                      name="address"
+                      onChange={this.handleChange}
+                      id="address"
                       placeholder="Address"
+                      required
                     ></textarea>
                   </div>
-                  <div className="form-row row1 mob">
-                    <div className="col-12 col-md-4  no-gutters">
-                      <select class="form-control col" id="Student_input">
-                        <option disabled selected>
-                          Gender
-                        </option>
-                        <option>Male</option>
-                        <option>Female</option>
-                      </select>
-                    </div>
-                    <div className="col-12 col-md-4 no-gutters">
-                      <input
-                        type="date"
-                        class="form-control col "
-                        id="Student_input"
-                        placeholder="Date of Birth"
-                      />
-                    </div>
-                    <div className="col-2"></div>
-                  </div>
                   <div className="form-row row1 desk">
-                    <div className="col-md-4 col-12 no-gutters">
+                    <div className="col-md-4 col-12 no-gutters StudentMobile_mp">
                       <input
                         type="date"
-                        class="form-control col "
-                        id="Student_input"
+                        class="Student_input form-control col "
+                        id="DateOfBirth"
+                        name="DateOfBirth"
+                        onChange={this.handleChange}
                         placeholder="Date of Birth"
+                        required
                       />
                     </div>
                     <div className="col-md-4 col-12 no-gutters">
                       <select
-                        class="form-control col"
-                        id="Student_input"
+                        class="Student_input form-control StudentMobile_mp"
+                        id="bloodGroup"
+                        required
+                        name="bloodGroup"
+                        onChange={this.handleChange}
                         placeholder="Blood Group"
                       >
-                        <option disabled selected>
+                        <option value="" disabled selected>
                           Blood Group
                         </option>
-                        <option>A+</option>
-                        <option>B+</option>
-                        <option>O+</option>
-                        <option>A-</option>
-                        <option>B-</option>
-                        <option>O-</option>
-                        <option>B+</option>
-                        <option>B+</option>
+                        <option value="A+">A+</option>
+                        <option value="B-">B+</option>
+                        <option value="O-">O+</option>
+                        <option value="A-">A-</option>
+                        <option value="B-">B-</option>
+                        <option value="O-">O-</option>
+                        <option value="AB+">AB+</option>
+                        <option value="AB-">AB-</option>
                       </select>
                     </div>
-                    <div className="col-md-4 col-12 no-gutters">
-                      <select class="form-control col" id="Student_input">
-                        <option disabled selected>
+                    <div className="col-md-4 col-12 no-gutters StudentMobile_mp">
+                      <select
+                        class="form-control Student_input"
+                        id="gender"
+                        required
+                        name="gender"
+                        onChange={this.handleChange}
+                      >
+                        <option disabled value="" selected>
                           Gender
                         </option>
-                        <option>Male</option>
-                        <option>Female</option>
+                        <option value="male">Male</option>
+                        <option value="female">Female</option>
                       </select>
                     </div>
                   </div>
                   <div className="form-row row1">
-                    <div className="col-md-4 col-12 no-gutters ">
+                    <div className="col-md-4 col-12 no-gutters StudentMobile_mp">
                       <input
-                        type="number"
-                        class="form-control  "
-                        id="Student_input"
+                        class="form-control Student_input"
+                        id="contactNo"
+                        name="contactNo"
+                        required
                         placeholder="Mobile Number 1"
+                        pattern="[1-9]{1}[0-9]{9}"
+                        title="Number should be of 10 digits only"
+                        type="tel"
+                        onChange={this.handleChange}
                       />
                     </div>
-                    <div className="col-md-4 col-12 no-gutters ">
+                    <div className="col-md-4 col-12 no-gutters StudentMobile_mp">
                       <input
-                        type="number"
-                        class="form-control "
-                        id="Student_input"
+                        class="form-control Student_input"
+                        id="alternateNo"
+                        name="alternateNo"
+                        pattern="[1-9]{1}[0-9]{9}"
+                        title="Number should be of 10 digits only"
+                        type="tel"
+                        required
+                        onChange={this.handleChange}
                         placeholder="Mobile Number 2"
                       />
                     </div>
                     <div class="col"></div>
                   </div>
-                  <div className="form-row row3">
+                  <div className="form-row row3 StudentMobile_mp">
                     <input
                       type="email"
-                      class="form-control col-12 col-md-8 no-gutters"
-                      id="Student_input"
+                      name="email"
+                      id="email"
+                      onChange={this.handleChange}
+                      required
+                      class="form-control col-12 col-md-8 no-gutters Student_input"
                       placeholder="Email-id"
                     />
                     {/* <input type="name" class="form-control col" id="Student_input" placeholder="Mobile Number 2"/>
                                 <div  class="col" ></div> */}
                   </div>
-                  <div className="form-row row1 mob">
-                    <div className="col-5 no-gutters">
-                      <select
-                        class="form-control col"
-                        id="Student_input"
-                        placeholder="Blood Group"
-                      >
-                        <option disabled selected>
-                          Blood Group
-                        </option>
-                        <option>A+</option>
-                        <option>B+</option>
-                        <option>O+</option>
-                        <option>A-</option>
-                        <option>B-</option>
-                        <option>O-</option>
-                        <option>B+</option>
-                        <option>B+</option>
-                      </select>
-                    </div>
-                  </div>
+
                   <div>
                     <button
-                      className="Student_next"
+                      type="button"
+                      className="Student_next StudentMobile_mp"
                       onClick={this.handleClickNext}
                     >
                       NEXT
@@ -268,34 +333,42 @@ export default class StudentForm extends React.Component {
                 <div className="Student_head2" id="desktop">
                   You are so close
                 </div>
-                <div className="form-row row1">
+                <div className="form-row row1 StudentMobile_mp">
                   <input
-                    class="form-control col "
-                    id="Student_input"
+                    type="text"
+                    class="form-control col-12 Student_input"
+                    id="collegeName"
+                    name="collegeName"
                     placeholder="College Name"
                   />
                 </div>
                 <div className="form-row row1">
-                  <div className="col-md-4 col-12 no-gutters ">
-                    <select class="form-control col" id="Student_input">
-                      <option disabled selected>
+                  <div className="col-md-4 col-12 no-gutters StudentMobile_mp">
+                    <select
+                      class="form-control Student_input"
+                      id="yearOfPassing"
+                      name="yearOfPassing"
+                    >
+                      <option disabled value="" selected>
                         Year of Passing
                       </option>
-                      <option>2019</option>
-                      <option>2020</option>
+                      <option value="2019">2019</option>
+                      <option value="2020">2020</option>
                     </select>
                   </div>
-                  <div className="col-md-4 col-12 no-gutters ">
+                  <div className="col-md-4 col-12 no-gutters StudentMobile_mp">
                     <input
-                      type="name"
-                      class="form-control col"
-                      id="Student_input"
+                      type="text"
+                      class="form-control Student_input"
+                      id="memberType"
+                      onChange={this.handleChange}
                       placeholder="Member Type"
+                      name="memberType"
                     />
                   </div>
-                  <div class="col"></div>
                 </div>
-                <div className="form-row row4 align-items-end">
+
+                {/*  <div className="form-row row4 align-items-end">
                   <div className="border-capcha">
                     <div className="Student_cap">Capcha</div>
                     <div className="col Student_capcha">RTX3090</div>
@@ -305,9 +378,14 @@ export default class StudentForm extends React.Component {
                     placeholder="Enter Capcha"
                   />
                   <div className="col"></div>
-                </div>
+                </div> */}
+                <Recaptcha
+                  ref={(ref) => (this.recaptcha = ref)}
+                  sitekey="6Lfj_U0aAAAAAKu_W5IjQrRS2BHjRypOQCUT74qU"
+                  onResolved={this.onResolved}
+                />
                 <div className="row5 form-row row align-items-center">
-                  <div className="col-2">
+                  <div className="col-md-3 col-2 Student_BackBtn">
                     <a onClick={this.handleClick} className="">
                       <span>
                         <img src={arrow}></img>{" "}
@@ -315,8 +393,15 @@ export default class StudentForm extends React.Component {
                       Back
                     </a>
                   </div>
-                  <div className="col-10">
-                    <button className="Student_submit"> Submit</button>
+                  <div className="col-md-8 col-9">
+                    <button
+                      type="submit"
+                      onClick={this.handleSubmit}
+                      className="Student_submit"
+                    >
+                      {" "}
+                      Submit
+                    </button>
                   </div>
                 </div>
                 <div className="row form-row d-flex justify-content-end">
